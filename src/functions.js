167c0848b6b7ad2b042b7887f2a890793e61bb96
agunbuhori <agun.buhori1@gmail.com>
2018-08-23 00:16:53 +0700

    Fraction item revision

--- src/functions.js
+++ src/functions.js
@@ -5,7 +5,7 @@
  * @since 2018
  */
 
-const fractions = [100000, 50000, 20000, 10000, 5000, 1000, 500, 100, 50, 10];
+const fractions = [100000, 50000, 20000, 10000, 5000, 1000, 500, 100, 50];
 const validCurrencyPattern = /^(Rp)?( )?((\d{1,3}){1}((\.\d{3})+(\,00)?)|\d+)$/;
 const currencyCharsPattern = /[Rp\. ]+/g;
 
